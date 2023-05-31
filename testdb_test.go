7003af9c8241f90945e2a7a9c63c4e7acb6301b6
Peter Downs <github@peterdowns.com>
2023-05-30 22:43:42 -0400

    Test CREATE INDEX CONCURRENTLY

--- testdb_test.go
+++ testdb_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/peterldowns/testdb"
+	"github.com/peterldowns/testdb/migrators/common"
 
 	"github.com/peterldowns/testy/assert"
 	"github.com/peterldowns/testy/check"
@@ -240,4 +241,80 @@ func TestDifferentHashesAlwaysResultInDifferentDatabases(t *testing.T) {
 	if check.Nil(t, err) {
 		check.Equal(t, 0, countXXX)
 	}
+}
+
+// This test confirms that due to testdb's locking strategy, even a migrator
+// that uses advisory locks and runs a migration with "CREATE INDEX CONCURRENTLY"
+// will succeed. testdb will take an advisory lock on the primary database
+// that it is connected to, NOT on the template database. This means that there
+// is only ever one migrator running on the template database at a time, and there
+// will never be any other migrators waiting or potentially contending an advisory
+// lock on that database.
+//
+// Normally, if you have two connections to a database (c1 and c2), and they are
+// contending an advisory lock, attempting to CREATE INDEX CONCCURENTLY will
+// cause a deadlock error:
+//
+//	C1: SELECT pg_advisory_locK(1111) -- returns OK
+//	C2: SELECT pg_advisory_locK(1111) -- hangs indefinitely, waiting on C1
+//	C1: CREATE INDEX CONCURRENTLY ... -- fails with warning about deadlock, waiting on
+//	                                  -- the C2 virtual transaction!
+//
+// Here, that's not an issue because we serialize the creation of the template
+// database, and C2 will never exist :)
+func TestMigrationWithConcurrentCreate(t *testing.T) {
+	t.Parallel()
+	config := testdb.Config{
+		User:     "postgres",
+		Password: "password",
+		Host:     "localhost",
+		Port:     "5433",
+		Options:  "sslmode=disable",
+	}
+	migrator := &sqlMigrator{
+		migrations: []string{
+			"CREATE TABLE users (id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY)",
+			"CREATE INDEX CONCURRENTLY example_concurrent_index ON users (id)",
+		},
+	}
+	for i := 0; i < 10; i++ {
+		t.Run(fmt.Sprintf("subtest_%d", i), func(t *testing.T) {
+			_ = testdb.New(t, config, migrator)
+		})
+	}
+}
+
+type sqlMigrator struct {
+	migrations []string
+}
+
+func (s *sqlMigrator) Hash() (string, error) {
+	hash := common.NewRecursiveHash()
+	for _, migration := range s.migrations {
+		hash.Add([]byte(migration))
+	}
+	return hash.String(), nil
+}
+
+func (s *sqlMigrator) Migrate(ctx context.Context, db *sql.DB, _ testdb.Config) error {
+	if _, err := db.ExecContext(ctx, "SELECT pg_advisory_lock(1111)"); err != nil {
+		return err
+	}
+	for _, migration := range s.migrations {
+		if _, err := db.ExecContext(ctx, migration); err != nil {
+			return err
+		}
+	}
+	if _, err := db.ExecContext(ctx, "SELECT pg_advisory_unlock(1111)"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (*sqlMigrator) Prepare(_ context.Context, _ *sql.DB, _ testdb.Config) error {
+	return nil
+}
+
+func (*sqlMigrator) Verify(_ context.Context, _ *sql.DB, _ testdb.Config) error {
+	return nil
 }
